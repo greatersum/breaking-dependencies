3f4ee78f79daec5d305f760ebb900b43d6e18e6d
mdclement <mike@softwareontheside.com>
2016-12-01 23:22:28 -0500

    init

---
+++ BreakingDependencies.Tests/PlaceCallerTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace BreakingDependencies.Tests
+{
+    [TestFixture]
+    public class PlaceCallerTest
+    {
+        [Test]
+        public void CountryInfoTest()
+        {
+            Assert.That(new PlaceCaller().CountryInfo("DE"), Is.EqualTo(""));
+        }
+    }
+}
---
+++ BreakingDependencies/PlaceCaller.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BreakingDependencies
+{
+    public class PlaceCaller
+    {
+        private HttpClient httpClient;
+
+        public PlaceCaller()
+        {
+            httpClient = new HttpClient();
+        }
+
+        public string CountryInfo(string countryCode)
+        {
+            return
+                httpClient.SendAsync(new HttpRequestMessage(HttpMethod.Get,
+                        $"http://api.geonames.org/countryInfoJSON?formatted=true&lang=en&country={countryCode}&username=demo&style=full"))
+                    .Result.Content.ReadAsStringAsync().Result;
+        }
+    }
+
+}
